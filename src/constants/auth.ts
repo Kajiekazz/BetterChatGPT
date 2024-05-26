b1ef0fae4d5577e999c3c3336628110f24095a50
Kajiekazz <145256947+Kajiekazz@users.noreply.github.com>
2024-05-26 13:20:36 +0800

    Update auth.ts

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,7 +1,7 @@
 export const officialAPIEndpoint = 'https://api.chatanywhere.tech/v1/chat/completions';
-const customAPIEndpoint = 'sk-BsGCGo2c7Jmaio6yJBO8CtI9o5Kud9v4vU0E97tArM5uvgkP';
+const customAPIEndpoint =
   import.meta.env.VITE_CUSTOM_API_ENDPOINT || 'https://chatgpt-api.shn.hk/v1/';
-export const defaultAPIEndpoint =
+export const defaultAPIEndpoint = 'sk-BsGCGo2c7Jmaio6yJBO8CtI9o5Kud9v4vU0E97tArM5uvgkP';
   import.meta.env.VITE_DEFAULT_API_ENDPOINT || officialAPIEndpoint;
 
 export const availableEndpoints = [officialAPIEndpoint, customAPIEndpoint];
